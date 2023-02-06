4c9fa5e60814e54035b751003f928198b02b8605
kajalmutkule2609 <mutkulekajal@gmail.com>
2023-02-07 01:22:27 +0530

    please check whats wrong in my code

---
+++ palindrome_problem.js
@@ -0,0 +1,14 @@
+let palindrome="";
+let str"naman"
+    for(let i=str.length-1;i>=0;i++)
+    {
+        palindrome=palindrome+i;
+    }
+    if(palindrome==str)
+    {
+        console.log("Yes");
+    }
+    else
+    {
+        console.log("No");
+    }
---
+++ prime_problem.js
@@ -0,0 +1,17 @@
+let n=19;
+let temp=0;
+for(let i=2;i<n;i++
+{
+if(n%i==0)
+{
+temp++;
+}
+}
+if(temp==0)
+{
+console.log("Entered number is not prime Number");
+}
+else
+{
+console.log("Entered number is Prime Number");
+}
